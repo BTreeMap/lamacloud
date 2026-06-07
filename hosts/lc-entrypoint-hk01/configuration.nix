8b5ef9cc747be8e7969407b00dfa232fe907f792
Joe Fang (via Code Server) <25814618+MinecraftFuns@users.noreply.github.com>
2026-06-07 11:41:27 -0400

    feat: network stack optimization

--- hosts/lc-entrypoint-hk01/configuration.nix
+++ hosts/lc-entrypoint-hk01/configuration.nix
@@ -7,4 +7,13 @@
     serverctl
     nodejs nginx certbot
   ];
+
+  boot.kernel.sysctl = {
+    "net.core.default_qdisc" = "fq";
+    "net.ipv4.tcp_congestion_control" = "bbr";
+    "net.core.rmem_max" = 7500000;
+    "net.core.wmem_max" = 7500000;
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.conf.all.forwarding" = 1;
+  };
 })
